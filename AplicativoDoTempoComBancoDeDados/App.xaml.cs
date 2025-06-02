9fda9120214c001b2234f6f7a21c42d032edc0d4
BrunoFreitas09 <brunootaviocostadefreitas@gmail.com>
2025-06-02 14:49:04 -0300

    Recomecando o projeto

--- AplicativoDoTempoComBancoDeDados/App.xaml.cs
+++ AplicativoDoTempoComBancoDeDados/App.xaml.cs
@@ -1,15 +1,32 @@
-﻿namespace AplicativoDoTempoComBancoDeDados
+﻿using AplicativoDoTempoComBancoDeDados.Helpers;
+
+namespace AplicativoDoTempoComBancoDeDados
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper _db;
+        public static SQLiteDatabaseHelper Db
+        {
+            get
+            {
+                if (_db == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                        "banco_sqlite_tempo.db3");
+
+                    _db = new SQLiteDatabaseHelper(path);
+                }
+
+                return _db;
+            }
+        }
         public App()
         {
             InitializeComponent();
         }
 
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            return new Window(new AppShell());
-        }
+
     }
 }
