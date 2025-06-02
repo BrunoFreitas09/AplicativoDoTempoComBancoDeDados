5c242b4e4a939caf03103dc5f45dd1541a69913d
BrunoFreitas09 <brunootaviocostadefreitas@gmail.com>
2025-06-02 11:36:46 -0300

    Adicionar arquivos de projeto.

---
+++ AplicativoDoTempoComBancoDeDados/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace AplicativoDoTempoComBancoDeDados
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ AplicativoDoTempoComBancoDeDados/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AplicativoDoTempoComBancoDeDados
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
